7fc687d85a7614738f44cd4fe75582c471b1b2ce
ELLipSE <ellipse@bookingsystem.com>
2021-06-14 17:07:23 +0600

    first commit..

---
+++ src/components/FirstApp.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="app">
+        {{ user.username }} - {{ fullName }} <br>
+        <strong>Followers: </strong> {{ followers }} <br>
+        <button @click="followUser">
+            Follow
+        </button>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'App',
+    data () {
+        return{
+        followers: 0,
+        user: {
+                id: 1,
+                username: 'SagolMara',
+                firstName: 'Sagol',
+                lastName: 'Mara',
+                email: 'SagolMara@gmail.com',
+                isAdmin: true,
+            },
+        }
+    },
+    watch: {
+        followers(newFollowerCount, oldFollowerCount){
+            if(oldFollowerCount < newFollowerCount){
+                console.log(`${this.user.username} has gain a follower....`)
+            }
+        }
+    },
+    computed: {
+        fullName(){
+            return `${this.user.firstName} ${this.user.lastName}`
+        }
+    },
+    methods:{
+        followUser() {
+            this.followers++;
+        }
+    },
+    mounted(){
+        this.followUser;
+    }
+}
+</script>
+
+<style>
+#app {
+    margin: 50px auto;
+}
+</style>
